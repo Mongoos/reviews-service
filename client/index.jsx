e6178ec48d89957b91435778c92f1723ff938359
Chandra Nguyen <chandra.nguyen@outlook.com>
2020-08-10 22:24:58 -0600

    Implemented styling for client model, refactored data seeding to be more randomized, and returns averages for id 1

--- client/index.jsx
+++ client/index.jsx
@@ -21,28 +21,121 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={{
+        fontFamily:"Helvetica"
+      }}>
         <div className="review-header">
-        <h1>★ Avg Overall Review Rating ({this.state.reviews.numberOfReviews} reviews)</h1>
+        <h2 style={{padding:"10px"}}> <div style={{
+          display: "inline-block",
+          color: "red"
+        }}>★</div> Avg Overall Review Rating ({this.state.reviews.numberOfReviews} reviews)</h2>
         </div>
-        <div className="review-categories">
-          <div id="cleanliness-box">
-    <div>Cleanliness</div> <div> ----- </div> <div>{this.state.reviews.cleanliness}</div>
+        <div className="review-categories" style={{
+          display:"grid",
+          gridTemplateColumns:"40% 10% 40%"
+         }}>
+          <div id="cleanliness-box" style={{
+          display:"grid",
+          gridColumnStart: 1,
+          gridColumnEnd: 2,
+          gridTemplateColumns:"70% 20% 10%",
+          padding: "10px"
+         }}> <div style={{
+            gridColumnStart: 1,
+            gridColumnEnd: 2
+          }}>Cleanliness</div> <div style={{
+            gridColumnStart: 2,
+            gridColumnEnd: 3
+          }}>-----</div> <div  style={{
+            gridColumnStart: 3,
+            gridColumnEnd: 4
+          }}>{this.state.reviews.cleanliness}</div>
+
           </div>
-          <div id="accuracy-box">
-    <div>Accuracy</div> <div> ----- </div> <div>{this.state.reviews.accuracy}</div>
+          <div id="accuracy-box" style={{
+          display:"grid",
+          gridColumnStart: 3,
+          gridColumnEnd: 4,
+          gridTemplateColumns:"70% 20% 10%",
+          padding: "10px"
+         }}><div style={{
+          gridColumnStart: 1,
+          gridColumnEnd: 2
+        }}>Accuracy</div> <div style={{
+          gridColumnStart: 2,
+          gridColumnEnd: 3
+        }}>-----</div> <div style={{
+          gridColumnStart: 3,
+          gridColumnEnd: 4
+        }}>{this.state.reviews.accuracy}</div>
           </div>
-          <div id="communication-box">
-    <div>Communication</div> <div> ----- </div> <div>{this.state.reviews.communication}</div>
+          <div id="communication-box" style={{
+          display:"grid",
+          gridColumnStart: 1,
+          gridColumnEnd: 2,
+          gridTemplateColumns:"70% 20% 10%",
+          padding: "10px"
+         }}><div style={{
+            gridColumnStart: 1,
+            gridColumnEnd: 2
+          }}>Communication</div><div style={{
+            gridColumnStart: 2,
+            gridColumnEnd: 3
+          }}>-----</div><div style={{
+            gridColumnStart: 3,
+            gridColumnEnd: 4
+          }}>{this.state.reviews.communication}</div>
           </div>
-          <div id="location-box">
-    <div>Location</div> <div> ----- </div> <div>{this.state.reviews.location}</div>
+          <div id="location-box" style={{
+          display:"grid",
+          gridColumnStart: 3,
+          gridColumnEnd: 4,
+          gridTemplateColumns:"70% 20% 10%",
+          padding: "10px"
+         }}><div style={{
+            gridColumnStart: 1,
+            gridColumnEnd: 2
+          }}>Location </div><div style={{
+            gridColumnStart: 2,
+            gridColumnEnd: 3
+          }}>-----</div> <div style={{
+            gridColumnStart: 3,
+            gridColumnEnd: 4
+          }}>{this.state.reviews.location}</div>
           </div>
-          <div id="checkin-box">
-    <div>Check-in</div> <div> ----- </div> <div>{this.state.reviews.checkin}</div>
+          <div id="checkin-box" style={{
+          display:"grid",
+          gridColumnStart: 1,
+          gridColumnEnd: 2,
+          gridTemplateColumns:"70% 20% 10%",
+          padding: "10px"
+         }}><div style={{
+          gridColumnStart: 1,
+          gridColumnEnd: 2
+        }}>Check-in </div><div style={{
+          gridColumnStart: 2,
+          gridColumnEnd: 3
+        }}>-----</div> <div style={{
+          gridColumnStart: 3,
+          gridColumnEnd: 4
+        }}>{this.state.reviews.checkin} </div>
           </div>
-          <div id="value-box">
-    <div>Value</div> <div> ----- </div> <div>{this.state.reviews.value}</div>
+          <div id="value-box" style={{
+          display:"grid",
+          gridColumnStart: 3,
+          gridColumnEnd: 4,
+          gridTemplateColumns:"70% 20% 10%",
+          padding: "10px"
+         }}><div style={{
+          gridColumnStart: 1,
+          gridColumnEnd: 2
+        }}>Value </div><div style={{
+          gridColumnStart: 2,
+          gridColumnEnd: 3
+        }}>-----</div> <div style={{
+          gridColumnStart: 3,
+          gridColumnEnd: 4
+        }}>{this.state.reviews.value}</div>
           </div>
         </div>
         <div className="reviews">
